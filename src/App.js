f4e87bb9d2277bf9565b2ad79a26960f43e5af5a
SondreHal <104064056+SondreHal@users.noreply.github.com>
2022-10-07 13:13:23 +0200

    Revert "file structure"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import "./assets/Global.css";
-import HelloWorld from "./components/helloworld/HelloWorld";
+import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-			<HelloWorld />
-		</div>
-	);
+	return <div className="App"></div>;
 }
 
 export default App;
--- src/components/helloworld/style.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const HelloWorldContainer = styled.div`
-	color: white;
-`;
