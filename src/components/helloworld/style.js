0850a181f7ce5d0790a2c04f765036b8c13751cb
Sondre <sondre-h@hotmail.com>
2022-10-07 13:22:01 +0200

    styling

--- src/components/helloworld/style.js
+++ src/components/helloworld/style.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
-export const HelloWorldContainer = styled.div`
+export const HelloWorldContainer = styled.h1`
 	color: white;
+	margin: 10rem 0;
+	text-align: center;
 `;
