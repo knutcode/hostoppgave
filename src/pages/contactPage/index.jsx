a6a1d8826bc368be3bd393e0f370287100ff537e
Sondre <sondre-h@hotmail.com>
2022-10-11 10:57:54 +0200

    filestructure

---
+++ src/pages/contactPage/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactPage = () => {
+	return (
+		<section>
+			<div className="container">ContactPage</div>
+		</section>
+	);
+};
+
+export default ContactPage;
