9625a79b6355ee63efabb1ad9ab21351f956d28f
Sondre <sondre-h@hotmail.com>
2022-10-11 14:51:32 +0200

    navbar styling

--- src/pages/components/Navbar/Navbar.jsx
+++ src/pages/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router";
 import ContactPage from "../../contactPage";
 import ErrorPage from "../../errorPage";
@@ -7,6 +7,8 @@ import PlanetsPage from "../../planetsPage";
 import * as s from "./style";
 
 const Navbar = () => {
+	const [activeNav, setActiveNav] = useState("active");
+
 	return (
 		<>
 			<s.Nav>
@@ -15,9 +17,15 @@ const Navbar = () => {
 						Lo<s.LogoSpan>go</s.LogoSpan>
 					</h2>
 					<s.NavLinks>
-						<s.StyledLink to="/">Home</s.StyledLink>
-						<s.StyledLink to="/planets">Planets</s.StyledLink>
-						<s.StyledLink to="/contact">Contact</s.StyledLink>
+						<s.StyledLink to="/" className={activeNav}>
+							Home
+						</s.StyledLink>
+						<s.StyledLink to="/planets" className={activeNav}>
+							Planets
+						</s.StyledLink>
+						<s.StyledLink to="/contact" className={activeNav}>
+							Contact
+						</s.StyledLink>
 					</s.NavLinks>
 				</s.StyledNav>
 			</s.Nav>
