bbae3e60483f6eeab5b72596091540febc9f7241
Sondre <sondre-h@hotmail.com>
2022-10-11 11:47:39 +0200

    change homepage

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const HomePage = () => {
 	return (
 		<>
-			<section>
+			<main>
 				<div className="container">HomePage</div>
-			</section>
+			</main>
 		</>
 	);
 };
