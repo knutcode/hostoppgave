abc087044eb4ea9e121d8c011f6ad2fb5552cc80
Trezlor <77504889+Trezlor@users.noreply.github.com>
2022-10-12 15:15:45 +0200

    added planet container

--- src/pages/planetsPage/index.jsx
+++ src/pages/planetsPage/index.jsx
@@ -5,18 +5,28 @@ import * as s from "./style";
 const PlanetsPage = () => {
 	return (
 		<>
-			<div className="container">
+			<div className="container" style={{ padding: 0 }}>
 				<SideNavbar />
-				<s.Section id="sun"></s.Section>
-				<section id="mercury"></section>
-				<section id="venus"></section>
-				<section id="earth"></section>
-				<section id="moon"></section>
-				<section id="mars"></section>
-				<section id="jupiter"></section>
-				<section id="saturn"></section>
-				<section id="uranus"></section>
-				<section id="neptune"></section>
+				<s.Section id="sun">
+					<s.PlanetContainer>
+						<s.PlanetApiLinks>
+							<s.PlanetApiLink href="">api 1</s.PlanetApiLink>
+							<s.PlanetApiLink>api 2</s.PlanetApiLink>
+							<s.PlanetApiLink>api 3</s.PlanetApiLink>
+						</s.PlanetApiLinks>
+						<s.Planet />
+					</s.PlanetContainer>
+					<s.PlanetInfoContainer></s.PlanetInfoContainer>
+				</s.Section>
+				<s.Section id="mercury"></s.Section>
+				<s.Section id="venus"></s.Section>
+				<s.Section id="earth"></s.Section>
+				<s.Section id="moon"></s.Section>
+				<s.Section id="mars"></s.Section>
+				<s.Section id="jupiter"></s.Section>
+				<s.Section id="saturn"></s.Section>
+				<s.Section id="uranus"></s.Section>
+				<s.Section id="neptune"></s.Section>
 			</div>
 		</>
 	);
--- src/pages/planetsPage/style.js
+++ src/pages/planetsPage/style.js
@@ -1,18 +1,20 @@
 import styled from "styled-components";
 
+// Navbar on left side for the planets
 export const SideNav = styled.nav`
 	font-size: 2rem;
 	display: flex;
 	gap: 1.9rem;
 	flex-direction: column;
-	width: fit-content;
+	width: 7.8rem;
 	position: fixed;
 	top: 50%;
 	margin-block: 2.5rem;
 	transform: translateY(-50%);
 
+	/* Links in navbar */
 	& a {
-		color: var(--color-white);
+		font-weight: 200;
 		transition: var(--transition);
 
 		&:hover {
@@ -26,6 +28,40 @@ export const SideNav = styled.nav`
 	}
 `;
 
+// Section for each planet
 export const Section = styled.section`
-	margin-top: -5rem;
+	display: flex;
+	margin-left: 7.8rem;
+`;
+
+// Planet Container
+export const PlanetContainer = styled.div`
+	/* background-color: red; */
+	width: 50%;
+	display: flex;
+	gap: 1rem;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const PlanetApiLinks = styled.div`
+	display: flex;
+	gap: 1rem;
+	margin-top: 5rem;
+`;
+
+export const PlanetApiLink = styled.h3``;
+
+export const Planet = styled.div`
+	width: 31rem;
+	aspect-ratio: 1/1;
+	background-color: blue;
+	border-radius: 50%;
+`;
+
+// Planet Info Container
+export const PlanetInfoContainer = styled.div`
+	background-color: green;
+	width: 50%;
 `;
