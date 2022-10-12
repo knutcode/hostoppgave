8a850695963893b8752589af8a7577d5d39cc832
kodehodeknut <knutc.kodehode@gmail.com>
2022-10-12 17:42:25 +0200

    test

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import * as s from "./style";
+import React from 'react';
+import * as s from './style';
 
 const HomePage = () => {
 	return (
 		<>
-			<s.Main>
-				<div className="container">HomePage</div>
-			</s.Main>
+			<section>
+				<div className="container">
+					<s.HeroContainer>
+						<s.Headline>Headline</s.Headline>
+						<s.SubHeader>SubHeader</s.SubHeader>
+					</s.HeroContainer>
+
+					<s.HeroLine></s.HeroLine>
+
+					<s.PlanetContainer>PlanetContainer</s.PlanetContainer>
+				</div>
+			</section>
 		</>
 	);
 };
--- src/pages/homePage/style.js
+++ src/pages/homePage/style.js
@@ -1,5 +1,17 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Main = styled.main`
-	min-height: 100vh;
+	/* min-height: 100vh; */
+`;
+
+export const HeroContainer = styled.div``;
+
+export const PlanetContainer = styled.div``;
+
+export const Headline = styled.h1``;
+
+export const SubHeader = styled.h2``;
+
+export const HeroLine = styled.div`
+	border-top: 3px solid white;
 `;
