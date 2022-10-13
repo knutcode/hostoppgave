eeee87ffa966fbac3e6171ec51c30facea0d56c3
AlexanderKodehode <93523917+AlexanderKodehode@users.noreply.github.com>
2022-10-13 12:35:52 +0200

    addet som comments

--- src/pages/contactPage/index.jsx
+++ src/pages/contactPage/index.jsx
@@ -7,19 +7,22 @@ const ContactPage = () => {
 	return (
 		<section>
 			<Flexbox flexDirection="row">
+				{/* Delen hvor vi lager boxen som man kan skriv inn i */}
 				<s.ContactLeft>
-					<s.User type="text" placeholder="Name" />
+					<s.User type="text" placeholder="Name" />		
 					<s.User type="text" placeholder="Mail"/>
 					<s.Comment type="text" placeholder="Comment" />
 					<br />
 					<button>Submit</button>
 				</s.ContactLeft>
 				<s.ContactRight>
+				{/* Delen hvor vi gir vår kontakt informasjon */}
 					<s.ContactInfo>
 						<h1>Contact information</h1>
 						<h2>+47 123 45 678</h2>
 						<h2>Hjemmeaddressen 15B, Bergen</h2>
 					</s.ContactInfo>
+				{/* Delen hvor vi gir muligheter å nå oss på */}
 					<s.ContactLink>
 						<h1>Find us at</h1>
 						<s.Link href="">Discord</s.Link>
