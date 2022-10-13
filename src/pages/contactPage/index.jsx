1ee33b9f5587d671faa0213af420340e1e0254c9
AlexanderKodehode <93523917+AlexanderKodehode@users.noreply.github.com>
2022-10-13 10:28:41 +0200

    Started makeing the contactpage

--- src/pages/contactPage/index.jsx
+++ src/pages/contactPage/index.jsx
@@ -1,12 +1,36 @@
 import React from "react";
-// import * as s from "./style";
+import Flexbox from 'flexbox-react';
+import * as s from "./style";
 
 const ContactPage = () => {
+
 	return (
 		<section>
-			<div className="container">ContactPage</div>
+			<Flexbox flexDirection="row">
+				<s.ContactLeft>
+					<s.user type="text" placeholder="Name" />
+					<s.user type="text" placeholder="Mail"/>
+					<s.comment type="text" placeholder="Comment" />
+					<br />
+					<button>Submit</button>
+				</s.ContactLeft>
+				<s.ContactRight>
+					<s.ContactInfo>
+						<h1>Contact information</h1>
+						<h2>+47 123 45 678</h2>
+						<h2>Hjemmeaddressen 15B, Bergen</h2>
+					</s.ContactInfo>
+					<s.ContactLink>
+						<h1>Find us at</h1>
+						<s.link href="">Discord</s.link>
+						<s.link href="">Twitter</s.link>
+						<s.link href="">Linkedin</s.link>
+					</s.ContactLink>
+				</s.ContactRight>
+			</Flexbox>
 		</section>
 	);
 };
 
 export default ContactPage;
+
--- src/pages/contactPage/style.js
+++ src/pages/contactPage/style.js
@@ -1,1 +1,45 @@
-// import styled from "styled-components";
+import styled from "styled-components";
+
+export const ContactLeft = styled.div`
+    width: 50vw;
+    height: 55vh;
+    margin: 130px 10px 0px 100px;
+    // border: 5px red solid;
+`
+
+export const ContactRight = styled.div`
+    width: 50vw;
+    height: 55vh;
+    margin: 130px 10px 0px 100px;
+    // border: 5px red solid;
+`
+
+export const ContactInfo = styled.div`
+    margin-bottom: 100px;
+    // border: 5px red solid;
+`
+
+export const ContactLink = styled.div`
+    // border: 5px red solid;
+`
+
+export const link = styled.a`
+    margin: 5px;
+`
+export const fromText = styled.input`
+    color: var(--color-primary);
+`
+
+export const user = styled.input`
+    color: var(--color-primary);
+    background-color: var(--color-bg);
+    border-bottom: var(--color-primary) 2px solid;
+    width: 90%;
+`
+export const comment = styled.input`
+    color: var(--color-primary);
+    background-color: var(--color-bg);
+    border: var(--color-primary) 2px solid;
+    padding: 15%;
+    width: 90%;
+`
