b18514908529005ccacc1faed3a096ede7850939
kodehodeknut <knutc.kodehode@gmail.com>
2022-10-14 11:48:43 +0200

    fix/refactoring and syntax fixes

--- src/pages/contactPage/index.jsx
+++ src/pages/contactPage/index.jsx
@@ -1,42 +1,56 @@
-import React from "react";
-import Flexbox from 'flexbox-react';
-import * as s from "./style";
+import React from 'react';
+import * as s from './style';
 import { AiOutlineLinkedin, AiOutlineTwitter } from 'react-icons/ai';
-import {BsDiscord} from 'react-icons/bs'
-import SubmitForm from "./Submit";
+import { BsDiscord } from 'react-icons/bs';
+import SubmitForm from './Submit';
 
 const ContactPage = () => {
-
 	return (
 		<section>
-			<Flexbox flexDirection="row">
-				{/* Delen hvor vi lager boxen som man kan skriv inn i */}
-				<s.ContactLeft>
-					<s.User type="text" placeholder="Name" />		
-					<s.User type="email" placeholder="Mail"/>
-					<s.Comment placeholder="Comment" />
-					<br />
-					<s.SubmitBTN onClick={SubmitForm}>Submit</s.SubmitBTN>
-				</s.ContactLeft>
-				<s.ContactRight>
-				{/* Delen hvor vi gir vår kontakt informasjon */}
-					<s.ContactInfo>
-						<s.Titel>Contact information</s.Titel>
-						<s.Text>+47 123 45 678</s.Text>
-						<s.Text>Hjemmeaddressen 15B, Bergen</s.Text>
-					</s.ContactInfo>
-				{/* Delen hvor vi gir muligheter å nå oss på */}
-					<s.ContactLink>
-						<s.Titel>Find us at</s.Titel>
-						<s.Link href=""> <BsDiscord /> </s.Link>
-						<s.Link href=""> <AiOutlineTwitter /> </s.Link>
-						<s.Link href=""> <AiOutlineLinkedin /> </s.Link>
-					</s.ContactLink>
-				</s.ContactRight>
-			</Flexbox>
+			<div className="container">
+				<s.ContactContainer>
+					{/* Delen hvor vi lager boxen som man kan skriv inn i */}
+					<s.ContactLeft>
+						<s.User
+							type="text"
+							placeholder="Name"
+						/>
+						<s.User
+							type="email"
+							placeholder="Mail"
+						/>
+						<s.Comment placeholder="Comment" />
+						<br />
+						<s.SubmitBtn onClick={SubmitForm}>Submit</s.SubmitBtn>
+					</s.ContactLeft>
+					<s.ContactRight>
+						{/* Delen hvor vi gir vår kontakt informasjon */}
+						<s.ContactInfo>
+							<s.Title>Contact information</s.Title>
+							<s.Text>+47 123 45 678</s.Text>
+							<s.Text>Hjemmeaddressen 15B, Bergen</s.Text>
+						</s.ContactInfo>
+						{/* Delen hvor vi gir muligheter å nå oss på */}
+						<s.ContactLink>
+							<s.Title>Find us at</s.Title>
+							<s.Link href="">
+								{' '}
+								<BsDiscord />{' '}
+							</s.Link>
+							<s.Link href="">
+								{' '}
+								<AiOutlineTwitter />{' '}
+							</s.Link>
+							<s.Link href="">
+								{' '}
+								<AiOutlineLinkedin />{' '}
+							</s.Link>
+						</s.ContactLink>
+					</s.ContactRight>
+				</s.ContactContainer>
+			</div>
 		</section>
 	);
 };
 
 export default ContactPage;
-
--- src/pages/contactPage/style.js
+++ src/pages/contactPage/style.js
@@ -1,70 +1,69 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+
+export const ContactContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	@media (max-width: 1090px) {
+		flex-direction: column;
+	} ;
+`;
 
 export const ContactLeft = styled.div`
-    width: 50vw;
-    height: 55vh;
-    margin: 130px 0px 0px 70px;
-    // border: 5px red solid;
-`
+	margin: 130px 0px 0px 70px;
+`;
 
 export const ContactRight = styled.div`
-    width: 50vw;
-    height: 55vh;
-    margin: 130px 50px 0px 0px;
-    // border: 5px red solid;
-`
+	margin: 130px 50px 0px 0px;
+`;
 
 export const ContactInfo = styled.div`
-    margin-bottom: 5%;
-    text-align: right;
-    padding: 5%;
-    // border: 5px red solid;
-`
+	margin-bottom: 3rem;
+	text-align: right;
+`;
 
 export const ContactLink = styled.div`
-    // border: 5px red solid;
-    text-align: right;
-    padding: 5%;
-`
+	text-align: right;
+`;
 
 export const Link = styled.a`
-    margin: 1%;
-    padding: 1%;
-    text-align: right;
-`
+	margin-inline: 0.5em;
+	text-align: right;
+	font-size: 24px;
+	color: var(--color-white);
+`;
 
 export const User = styled.input`
-    color: var(--color-primary);
-    background-color: var(--color-bg);
-    border-bottom: var(--color-primary) 2px solid;
-    width: 90%;
-    height: 10%;
-    margin-bottom: 5%;
-`
+	color: var(--color-primary);
+	background-color: var(--color-bg);
+	border-bottom: var(--color-primary) 2px solid;
+	width: 100%;
+	height: 2rem;
+	margin-bottom: 5%;
+`;
 export const Comment = styled.textarea`
-    color: var(--color-primary);
-    background-color: var(--color-bg);
-    border: var(--color-primary) 2px solid;
-    width: 90%;
-    height: 200px;
-    resize: none;
-`
+	color: var(--color-primary);
+	background-color: var(--color-bg);
+	border: var(--color-primary) 2px solid;
+	width: 100%;
+	height: 200px;
+	resize: none;
+`;
 
-export const Titel = styled.h1`
-    color: var(--color-primary);
-    font-size: 28px;
-    letter-spacing: 5px;
-`
+export const Title = styled.h1`
+	color: var(--color-primary);
+	font-size: 28px;
+	letter-spacing: 5px;
+`;
 export const Text = styled.h2`
-    font-size: 14px;
-    font-weight: 600;
-    letter-spacing: 3px;
-`
+	font-size: 14px;
+	font-weight: 600;
+	letter-spacing: 3px;
+`;
 
-export const SubmitBTN = styled.button`
-    border-radius: 60%;
-    height: 25px;
-    width: 90px;
-    color: var(--color-primary);
-    background-color: var(--color-white);
-`
+export const SubmitBtn = styled.button`
+	border-radius: 50px;
+	height: 25px;
+	width: 90px;
+	background-color: var(--color-white);
+`;
